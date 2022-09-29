83ce697adba6e7eb2038622d723e6618877a1a72
kakadmd <kakadmd@163.com>
2022-09-29 11:58:39 +0800

    feat:试一下代码冲突

--- src/router/modules/setting.js
+++ src/router/modules/setting.js
@@ -10,7 +10,7 @@ export default {
       component: () => import('@/views/setting'),
       meta: {
         icon: 'setting',
-        title: '公司设置' // meta属性的里面的属性 随意定义 但是这里为什么要用title呢， 因为左侧导航会读取我们的路由里的meta里面的title作为显示菜单名称
+        title: '公司设置abc' // meta属性的里面的属性 随意定义 但是这里为什么要用title呢， 因为左侧导航会读取我们的路由里的meta里面的title作为显示菜单名称
       }
     }
   ]
