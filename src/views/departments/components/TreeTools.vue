93885f5e47498f972018f6072c58c3020a90be3a
kakadmd <kakadmd@163.com>
2022-09-29 16:31:59 +0800

    feat:公司组织架构

---
+++ src/views/departments/components/TreeTools.vue
@@ -0,0 +1,48 @@
+<template>
+  <el-row
+    type="flex"
+    justify="space-between"
+    align="middle"
+    style="height:40px;width:100%;"
+  >
+    <el-col>
+      <span>{{ treeNode.name }}</span>
+    </el-col>
+    <el-col :span="4">
+      <el-row type="flex">
+        <span style="margin-right:8px;">{{ treeNode.manager }}</span>
+        <el-dropdown>
+          <span>
+            操作<i class="el-icon-arrow-down el-icon--right" />
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>添加子部门</el-dropdown-item>
+            <el-dropdown-item v-if="isShow">编辑子部门</el-dropdown-item>
+            <el-dropdown-item v-if="isShow">删除子部门</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-row>
+    </el-col>
+  </el-row>
+
+</template>
+
+<script>
+export default {
+  name: 'HrsaasTreeTools',
+  props: {
+    treeNode: {
+      type: Object,
+      required: true
+    },
+    isShow: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,29 +1,75 @@
 <template>
-  <div>
-    departments
+  <div class="departments-container">
+    <el-card>
+      <TreeTools :tree-node="company" :is-show="false" />
+    </el-card>
+    <el-tree :data="departs" :props="defaultProps" :default-expand-all="true">
+      <TreeTools slot-scope="{data}" :tree-node="data" />
+    </el-tree>
   </div>
 </template>
 
 <script>
+import TreeTools from './components/TreeTools.vue'
+import { getDepartments } from '@/api/departments'
 export default {
   name: 'HrsaasIndex',
-
+  components: {
+    TreeTools
+  },
   data() {
     return {
-
+      company: {
+        name: '江苏传智播客教育科技股份有限公司',
+        manager: '负责人'
+      },
+      departs: [
+        {
+          name: '总裁办',
+          manager: '曹操',
+          children: [
+            {
+              name: '董事会',
+              manager: '曹丕'
+            }
+          ]
+        },
+        {
+          name: '行政部',
+          manager: '刘备'
+        },
+        {
+          name: '人事部',
+          manager: '孙权'
+        }
+      ],
+      defaultProps: {
+        // label 	指定节点标签为节点对象的某个属性值
+        label: 'name'
+        // childen 指定子树为节点对象的某个属性值
+        // children: 'children'
+      }
     }
   },
 
   mounted() {
-
+    this.getDepartments()
   },
 
   methods: {
-
+    async getDepartments() {
+      const res = await getDepartments()
+      console.log(res)
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+
+.departments-container {
+  width: 900px;
+  margin: 20px auto;
+}
 
 </style>
